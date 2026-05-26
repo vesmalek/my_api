ddad7624f0ccfd86486ad99558585b13351b73b2
Ismail Malik <vesmalick@gmail.com>
2026-05-26 21:42:58 +0300

    Tested using Python requests

---
+++ test_body.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.post(
+    "http://127.0.0.1:8000/users",
+    json={
+        "username": "finito",
+        "email": "finito@busaraspace.com",
+        "age": 17,
+        "bio": "null",
+        "is_active": "false"
+    }
+)
+
+print(response.json())
